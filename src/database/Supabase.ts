90520efbd675eb5d5758e4ed074124481d1e2748
Seth Zaunbrecher <seth.zaunbrecher@gmail.com>
2025-10-05 13:13:17 -0500

    env vars

--- src/database/Supabase.ts
+++ src/database/Supabase.ts
@@ -1,7 +1,6 @@
 import {createClient} from '@supabase/supabase-js'
 
-
-const supaBaseUrl:any= import.meta.env.VITE_SUPABASE_URL
-const supaBaseKey:any = import.meta.env.VITE_SUPABASE_KEY
-
-export const supabase = createClient(supaBaseUrl,supaBaseKey)
+   const supaBaseUrl:string= import.meta.env.VITE_SUPABASE_URL
+   const supaBaseKey:string= import.meta.env.VITE_SUPABASE_KEY
+    
+    export const supabase = createClient(supaBaseUrl,supaBaseKey)
