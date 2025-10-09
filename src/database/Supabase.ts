305291aa4e1120db0e46f2e10b5537c13501ffbe
Seth Zaunbrecher <seth.zaunbrecher@gmail.com>
2025-10-09 09:49:48 -0500

    change

--- src/database/Supabase.ts
+++ src/database/Supabase.ts
@@ -1,12 +1,8 @@
 import {createClient} from '@supabase/supabase-js'
 
 const supaBaseUrl:string= import.meta.env.VITE_SUPABASE_URL
-let supaBaseKey:string="xxx"
+const supaBaseKey:string= import.meta.env.VITE_SUPABASE_KEY
 
 console.log(import.meta.env)
-if(import.meta.env.MODE=='development'){
 
-   supaBaseKey= import.meta.env.VITE_SUPABASE_KEY
-   
-}
 export const supabase = createClient(supaBaseUrl,supaBaseKey)
