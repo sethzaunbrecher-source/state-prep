f7406c1bada0bbbc2e8ab0b61b743071ed7ccdb1
Seth Zaunbrecher <seth.zaunbrecher@gmail.com>
2025-10-09 14:43:01 -0500

    changed env

--- src/database/Supabase.ts
+++ src/database/Supabase.ts
@@ -1,7 +1,7 @@
 import {createClient} from '@supabase/supabase-js'
 
-const supaBaseUrl:string= import.meta.env.VITE_SUPABASE_URL
-const supaBaseKey:string= import.meta.env.VITE_SUPABASE_KEY
+const supaBaseUrl:string= import.meta.env.SUPABASE_URL
+const supaBaseKey:string= import.meta.env.SUPABASE_KEY
 
 console.log(import.meta.env)
 
